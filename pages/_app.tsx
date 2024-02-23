c256b4932122552e9ae826a0d45ecb0eeac030ae
Christophe PETRE <cpetre@adimeo.com>
2024-02-23 07:59:58 +0100

    Fix : wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  console.log(pageProps.session);
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
